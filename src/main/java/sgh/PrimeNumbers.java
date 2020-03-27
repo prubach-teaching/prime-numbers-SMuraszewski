c4c7f90810de6a66e24e69e2712e539a8a50b5e7
Sebastian Muraszewski <sebmuraszewski@gmail.com>
2020-03-27 11:26:40 +0100

    Update PrimeNumbers.java

--- src/main/java/sgh/PrimeNumbers.java
+++ src/main/java/sgh/PrimeNumbers.java
@@ -3,10 +3,37 @@ package sgh;
 public class PrimeNumbers {
 
     public static void primes(int n) {
-        // Your code here
+        
+        StringBuilder output = new StringBuilder();
+        int count = 0;
+
+        int CheckingValue = 2;
+        while (count < n) {
+            if (isPrimeNumber(CheckingValue)) {
+                if (count == 0) {
+                    output.append(CheckingValue);
+                } else {
+                    output.append(", ").append(CheckingValue);
+                }
+                count++;
+            }
+            CheckingValue++;
+        }
+        
+        System.out.println("n = " + n + " " + output);
+
         System.out.println("2, 3, 5, 7");
     }
 
+
+   public static boolean isPrimeNumber(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         primes(4);
